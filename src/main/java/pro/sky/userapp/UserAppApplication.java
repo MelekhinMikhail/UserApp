66d14a4e4cafaceff82f120528d35a87281c803d
Admin <admin@fff.com>
2023-03-09 21:21:32 +0300

    Initial commit

---
+++ src/main/java/pro/sky/userapp/UserAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.userapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAppApplication.class, args);
+    }
+
+}
